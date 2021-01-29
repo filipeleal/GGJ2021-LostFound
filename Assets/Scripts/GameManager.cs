3caf636a7659052b21303ca0ca406a2607a8b8f4
Filipe Leal <leal.filipe@live.com>
2021-01-29 14:32:32 -0300

    First Load

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    [Range(1, 100)]
+    public int NumberOfSheeps;
+
+    public GameObject SpawnCenter;
+
+    public GameObject SheepPrefab;
+
+    public float Radius = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnSheeps();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnSheeps()
+    {
+        for (int i = 0; i < NumberOfSheeps; i++)
+        {
+            Vector3 pos = Random.insideUnitCircle * Radius;
+
+
+            var obj = Instantiate(SheepPrefab, SpawnCenter.transform.position + pos, Quaternion.identity, SpawnCenter.transform);
+            // Debug.Log(obj);
+            obj.transform.parent = SpawnCenter.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/SheepBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepBehaviour : MonoBehaviour
+{
+    public float WaitTime = 5;
+    public float Speed = 0.2f;
+
+    Vector3 pos;
+    bool isMoving = false;
+
+    float timeToMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToMove = Random.Range(1f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeToMove < 0 && !isMoving)
+        {
+            pos = (Random.insideUnitCircle * 0.2f);
+            pos += transform.position;
+            Move();
+            //timeToMove = WaitTime;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            Move();
+            timeToMove += Time.deltaTime;
+
+            if (timeToMove > WaitTime)
+            {
+                isMoving = false;
+                WaitTime = Random.Range(1f, 5f);
+            }
+        }
+        else
+        {
+            timeToMove -= Time.deltaTime;
+        }
+    }
+
+    void Move()
+    {
+        //Debug.Log(transform.parent);
+        float step = Speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, pos, step);
+    }
+}
